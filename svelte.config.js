7ccc61e6b2a935bdfd6cdc7895be4db9e34347d5
Connor Rothschild <connorrothschild@gmail.com>
2021-10-03 14:53:47 -0500

    nvm

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,8 @@ import adapter from "@sveltejs/adapter-netlify";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
-  preprocess: preprocess(),
-  // preprocess: mdsvex(mdsvexConfig),
+  // preprocess: preprocess(),
+  preprocess: mdsvex(mdsvexConfig),
   kit: {
     adapter: adapter(),
     target: "#svelte",
