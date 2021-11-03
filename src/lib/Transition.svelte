223446279496e965014ec1839097dc73895d3f6a
Connor Rothschild <connorrothschild@gmail.com>
2021-11-03 18:07:32 -0500

    remove all animations for users who prefer reduced animation

--- src/lib/Transition.svelte
+++ src/lib/Transition.svelte
@@ -2,7 +2,21 @@
   import { onMount } from "svelte";
   import { gsap } from "gsap";
 
+  let prefersReducedMotion = false;
+  onMount(() => {
+    let listener = window.matchMedia("(prefers-reduced-motion: reduce)");
+    prefersReducedMotion = listener.matches;
+
+    listener.addEventListener("change", () => {
+      prefersReducedMotion = listener.matches;
+      transition();
+    });
+
+    transition();
+  });
+
   function transition() {
+    if (prefersReducedMotion) return;
     gsap.fromTo(
       ".transition-title",
       { x: -10, opacity: 0 },
@@ -21,8 +35,4 @@
       { x: 0, opacity: 1, duration: 0.5, delay: 0.5, clearProps: "x" }
     );
   }
-
-  onMount(() => {
-    transition();
-  });
 </script>
