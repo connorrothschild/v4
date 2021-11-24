6e40bc1458e8f5324e79cbb5639cbfeb6921b6f0
Connor Rothschild <connorrothschild@gmail.com>
2021-11-24 16:27:59 -0600

    trying to fix jekyll thing

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,19 @@
-// import { preprocess } from 'svelte/compiler';
-// import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
 import { mdsvexConfig } from "./mdsvex.config.js";
-// import adapter from '@sveltejs/adapter-static';
 import adapter from "@sveltejs/adapter-netlify";
-// import image from 'svelte-image';
-
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
-  // 	preprocess: preprocess({
-  // 	...image(), ...mdsvex(mdsvexConfig)
-  // }),
   preprocess: [
-    // image(),
     mdsvex(mdsvexConfig),
   ],
   kit: {
-    adapter: adapter({}),
+    adapter: adapter({
+      pages: 'build',  
+      assets: 'build', 
+      fallback: null
+    }),
     target: "#svelte"
   },
 };
