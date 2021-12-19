27fc0ee2982a3d1d98087ba09d961f49d6e55c48
Connor Rothschild <connorrothschild@gmail.com>
2021-12-19 00:02:20 -0600

    run git config core.ignorecase false

---
+++ src/lib/Global/HighlightFromProp.svelte
@@ -0,0 +1,32 @@
+<script>
+  // FROM PARENT
+  export let hoveredIdProp;
+  export let highlightedLinesProp;
+
+  //   BACK TO PARENT
+  export let hoveredId;
+  export let highlightedLines;
+</script>
+
+<span
+  on:mouseover={() => {
+    hoveredId = hoveredIdProp;
+    highlightedLines = highlightedLinesProp;
+  }}
+  on:focus={() => {
+    hoveredId = hoveredIdProp;
+    highlightedLines = highlightedLinesProp;
+  }}
+  on:mouseleave={() => {
+    hoveredId = null;
+    highlightedLines = null;
+  }}><slot /></span
+>
+
+<style>
+  span {
+    background: rgba(var(--accent-color-rgb), 0.2);
+    padding: 1px 4px;
+    /* font-style: italic; */
+  }
+</style>
