dc6d33a0ba06325b7aa168fb4cb492225819a02e
Connor Rothschild <connorrothschild@gmail.com>
2024-01-02 12:57:19 -0600

    ?

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,13 +2,13 @@
 import { base } from "$app/paths";
 
 export async function load({ fetch }) {
-  const projectsJson = await fetch(`/v4/projects.json`);
+  const projectsJson = await fetch(`../v4/projects.json`);
   const projects = await projectsJson.json();
 
-  const postsJson = await fetch(`/v4/posts.json`);
+  const postsJson = await fetch(`../v4/posts.json`);
   const posts = await postsJson.json();
 
-  const awardsJson = await fetch(`/v4/awards.json`);
+  const awardsJson = await fetch(`../v4/awards.json`);
   const awards = await awardsJson.json();
 
   return {
--- src/routes/projects.json/+server.js
+++ src/routes/projects.json/+server.js
@@ -1,21 +1,19 @@
 export async function GET() {
-    const imports = import.meta.glob("../project/**/*.md");
-    let body = [];
+  const imports = import.meta.glob("../project/**/*.md");
+  let body = [];
 
-    for (const path in imports) {
-        body.push(
-            imports[path]().then(({ metadata }) => {
-                return {
-                    metadata: metadata,
-                    path: path,
-                };
-            })
-            );
-        }
-        
-    const projects = await Promise.all(body);
+  for (const path in imports) {
+    body.push(
+      imports[path]().then(({ metadata }) => {
+        return {
+          metadata: metadata,
+          path: path,
+        };
+      })
+    );
+  }
 
-    return new Response(
-        JSON.stringify(projects)
-    )
+  const projects = await Promise.all(body);
+
+  return new Response(JSON.stringify(projects));
 }
