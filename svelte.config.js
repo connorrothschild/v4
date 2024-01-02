4a84a2548cee6ce01df55b6f6e263d34e87dbc19
Connor Rothschild <connorrothschild@gmail.com>
2024-01-02 10:39:34 -0600

    ?

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
       fallback: "404.html",
     }),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/.com" : "",
+      base: process.env.NODE_ENV === "production" ? "" : "",
     },
   },
 };
